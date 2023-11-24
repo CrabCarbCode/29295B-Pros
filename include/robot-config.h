ad1d1414b0ac3683a268d4317f10fddc69681623
CrabianCode <odin.ravenworth@gmail.com>
2023-11-24 16:27:07 -0500

    Organization and Driver control code

--- include/robot-config.h
+++ include/robot-config.h
@@ -8,16 +8,17 @@
 pros::Controller MainControl (E_CONTROLLER_MASTER); //declared with "pros::" to avoid ambiguity with stock vex controller
 
 
-Motor LDriveFrontM (20, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-Motor LDriveBackM (10, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
-Motor RDriveFrontM (11, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-Motor RDriveBackM (1, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor LDriveFrontM (2, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+Motor LDriveBackM (1, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor RDriveFrontM (9, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+Motor RDriveBackM (8, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 
-Motor FlystickArmM (12, E_MOTOR_GEARSET_36, true, E_MOTOR_ENCODER_DEGREES);
-Motor FlystickWheelM (13, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+Motor FlystickArmM (6, E_MOTOR_GEARSET_36, true, E_MOTOR_ENCODER_DEGREES);
+Motor FlystickWheelM1 (13, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+Motor FlystickWheelM2 (15, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
 
 
-Imu Inertial(15); //initializing the Inertial sensor
+Imu Inertial(19); //initializing the Inertial sensor
 
 Motor_Group LDrive ({LDriveFrontM, LDriveBackM});
 Motor_Group RDrive ({RDriveFrontM, RDriveBackM});
